6ab5905b895a2305bb7f987c6274f659e5d6a816
Gaurav Mehta <gaurav_mehta@persistent.com>
2020-04-10 00:34:33 +0530

    dual Y axis and change of data to be plotted

--- js/amchart_linechart.js
+++ js/amchart_linechart.js
@@ -12,8 +12,10 @@ function cumulative_data() {
     chart.dataSource.parser = new am4core.CSVParser();
     chart.dataSource.parser.options.useColumnNames = true;
     chart.dataSource.parser.options.skipEmpty = false;
-    chart.dataSource.parser.options.numberFields = ["Total Beds Avail", "Total Projected Cases"];
+    chart.dataSource.parser.options.numberFields = ["Total Beds Needed", "Total Projected Cases"];
     chart.colors.step = 2;
+    // Converts Y axis values in K,M,B
+    chart.numberFormatter.numberFormat = "###a";
 
     // Create axes
     var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
@@ -22,9 +24,10 @@ function cumulative_data() {
     categoryAxis.renderer.labels.template.rotation = -45;
 
     // Create series
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-    function createAxisAndSeries(field, name, color) {
+    function createAxisAndSeries(field, name, color, opposite, bullet) {
+      var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
       var series = chart.series.push(new am4charts.LineSeries());
       series.dataFields.valueY = field;
       series.dataFields.categoryX = "date";
@@ -36,18 +39,51 @@ function cumulative_data() {
       series.tensionX = 0.8;
 
       series.showOnInit = true;
-      // if (field == 'Total Cases')
-      //   series.hidden = true;
 
-      var bullet = series.bullets.push(new am4charts.CircleBullet());
-      bullet.circle.stroke = color;
+      switch (bullet) {
+        case "triangle":
+          var bullet = series.bullets.push(new am4charts.Bullet());
+          bullet.width = 12;
+          bullet.height = 12;
+          bullet.horizontalCenter = "middle";
+          bullet.verticalCenter = "middle";
 
+          var triangle = bullet.createChild(am4core.Triangle);
+          triangle.stroke = color;
+          triangle.strokeWidth = 2;
+          triangle.direction = "top";
+          triangle.width = 12;
+          triangle.height = 12;
+          break;
+        case "rectangle":
+          var bullet = series.bullets.push(new am4charts.Bullet());
+          bullet.width = 10;
+          bullet.height = 10;
+          bullet.horizontalCenter = "middle";
+          bullet.verticalCenter = "middle";
+
+          var rectangle = bullet.createChild(am4core.Rectangle);
+          rectangle.stroke = color;
+          rectangle.strokeWidth = 2;
+          rectangle.width = 10;
+          rectangle.height = 10;
+          break;
+        default:
+          var bullet = series.bullets.push(new am4charts.CircleBullet());
+          bullet.circle.stroke = color;
+          bullet.circle.strokeWidth = 2;
+          break;
+      }
+
+      valueAxis.renderer.line.strokeOpacity = 1;
+      valueAxis.renderer.line.strokeWidth = 2;
+      valueAxis.renderer.line.stroke = series.stroke;
       valueAxis.renderer.labels.template.fill = am4core.color("#fff");
-      valueAxis.renderer.opposite = false;
+      valueAxis.renderer.opposite = opposite;
     }
 
-    createAxisAndSeries("Total Beds Avail", "Total Beds Avail", am4core.color("rgb(227, 74, 51)"));
-    createAxisAndSeries("Total Projected Cases", "Total Projected Cases", am4core.color("rgb(49, 163, 8)"));
+    createAxisAndSeries("Total Beds Needed", "Total Beds Needed", am4core.color("rgb(227, 74, 51)"), false, "rectangle");
+    createAxisAndSeries("Total Projected Cases", "Total Projected Cases", am4core.color("rgb(49, 163, 8)"), true, "circle");
 
     // Add legend
     chart.legend = new am4charts.Legend();
@@ -82,6 +118,7 @@ function daily_data() {
 
     // Create series
     var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
     function createAxisAndSeries(field, name, color) {
       var series = chart.series.push(new am4charts.LineSeries());
       series.dataFields.valueY = field;
